aa9f91e6002fb936e3aac8cd3e4f9f317e60bf0e
CHENG-TING CHEN <abfa762466@gmail.com>
2025-08-02 17:44:20 +0800

    fix: remove redundant folder
    I don't know why I upload this... I've grown up so I deleted it.

--- backend/dist/db.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prisma = void 0;
-const prisma_client_1 = require("prisma/prisma-client");
-const prisma = new prisma_client_1.PrismaClient();
-exports.prisma = prisma;
