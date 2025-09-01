03ee315e8d62f886e4b17ed03cf3274ac7b187f5
CHENG-TING CHEN <abfa762466@gmail.com>
2025-09-02 02:05:35 +0800

    feat: version I sql

--- backend/sql/V1__Initial.sql
+++ backend/sql/V1__Initial.sql
@@ -53,24 +53,45 @@ CREATE TABLE animals (
 
 CREATE TABLE owners (
 	id SERIAL PRIMARY KEY,
-	name VARCHAR(20) NOT NULL,
-	phone VARCHAR(15) NOT NULL,
-	email TEXT
+	name TEXT,
+	phone TEXT,
+	email TEXT,
+	CONSTRAINT unique_phone_email UNIQUE(phone, email)
 );
 
 CREATE TABLE animal_losts (
 	id SERIAL NOT NULL PRIMARY KEY,
-	chip_id text,
-	name VARCHAR(20),
-	kind VARCHAR(20),
-	variety VARCHAR(20),
-	sex VARCHAR(5),
-	colour VARCHAR(20),
-	outlook VARCHAR(20),
-	feature text,
-	lost_time date,
-	lost_place text,
-	picture text,
+	chip_id TEXT unique,
+	name TEXT,
+	kind TEXT,
+	variety TEXT,
+	sex TEXT,
+	colour TEXT,
+	outlook TEXT,
+	feature TEXT,
+	lost_time DATE,
+	lost_place TEXT,
+	picture TEXT,
 
 	owner_id INTEGER NOT NULL REFERENCES owners(id) ON DELETE CASCADE
 );
+
+-- FUNCTION
+-- CREATE OR REPLACE FUNCTION convert_sex_to_mf()
+-- RETURNS TRIGGER AS $$
+-- BEGIN
+--     IF NEW.sex = '公' THEN
+--        NEW.sex = 'M';
+--    ELSIF NEW.sex = '母' THEN
+--        NEW.sex = 'F';
+--    END IF;
+--
+--    RETURN NEW;
+-- END;
+-- $$ LANGUAGE plpgsql;
+
+-- TRIGGER
+-- CREATE TRIGGER convert_sex_trigger
+-- BEFORE INSERT OR UPDATE ON animal_losts
+-- FOR EACH ROW
+-- EXECUTE FUNCTION convert_sex_to_mf();
