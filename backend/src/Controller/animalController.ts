8dc4c8e68bb7f5c9c64346544fe786ad672fa601
CHENG-TING CHEN <abfa762466@gmail.com>
2025-09-15 02:09:56 +0800

    refactor: Divide updating data logic into proper pieces
    1. Follow Service - Repository - Controller

--- backend/src/Controller/animalController.ts
+++ backend/src/Controller/animalController.ts
@@ -1,21 +1,23 @@
 import express from 'express'
 
-import {
-	updateAnimalLostTable,
-	updateAnimalTable,
-} from '../libs/updateDatabase.utils';
 import AnimalRepository from '../repository/animal.db';
 import SuccessResponse from '../libs/successResponse';
 import * as apiMessage from '../libs/message'
 import CustomError from '../libs/customError';
 import DatabaseUtils from '../libs/database.utils';
 import AnimalHelper from './helper/animalHelper';
-import { Animal, AnimalOwner } from '../libs/zod/animals';
+import { Animal } from '../libs/zod/animals';
+import AnimalLostService from '../Service/animalLost';
+import AnimalService from '../Service/animal';
 
 class AnimalController {
-	animalRepository: AnimalRepository;
+	repository: AnimalRepository
+	service: AnimalLostService
+	animalService: AnimalService
 	constructor() {
-		this.animalRepository = new AnimalRepository();
+		this.repository = new AnimalRepository();
+		this.service = new AnimalLostService();
+		this.animalService = new AnimalService();
 	}
 
 	// TODO: Lack of prevCursor
@@ -26,7 +28,7 @@ class AnimalController {
 	) => {
 		const { parsedPageSize, id } = AnimalHelper.getQueryString(req)
 
-		const animals = await this.animalRepository.findAll<Animal>(parsedPageSize, id)
+		const animals = await this.repository.findAll<Animal>(parsedPageSize, id)
 		const { prevCursor, nextCursor } = DatabaseUtils.cursorPairGenerate(animals)
 
 		res.locals.result = new SuccessResponse('api',
@@ -48,7 +50,7 @@ class AnimalController {
 	) => {
 		const id = req.params.id as string
 
-		const animal = await this.animalRepository.findAnimalShelterById(id);
+		const animal = await this.repository.findAnimalShelterById(id);
 		if (!animal) {
 			throw new CustomError(apiMessage.ANIMAL_NOT_EXISTS);
 		}
@@ -64,7 +66,7 @@ class AnimalController {
 	) => {
 		const city = req.params.city as string
 
-		const animals = await this.animalRepository.findAnimalsByCity(city)
+		const animals = await this.repository.findAnimalsByCity(city)
 		res.locals.result = new SuccessResponse('api', { animals });
 		next()
 	}
@@ -74,12 +76,8 @@ class AnimalController {
 		res: express.Response,
 		next: express.NextFunction
 	) => {
-		// TODO: wrap in transaction
-		// const [animalTableinsertCount, animalLostTableCount] =
-		// 	await Promise.all([updateAnimalTable(), updateAnimalLostTable()]);
-
-		const animalTableinsertCount = await updateAnimalTable();
-		const animalLostTableCount = await updateAnimalLostTable();
+		const [animalTableinsertCount, animalLostTableCount] =
+			await Promise.all([this.animalService.updateAnimalTable(), this.service.updateTableAnimalLosts()]);
 
 		res.locals.result = new SuccessResponse('api', {
 			animalTables: animalTableinsertCount,
