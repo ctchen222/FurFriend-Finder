1d7e54686837bc7899f67f49589c995349757692
CHENG-TING CHEN <abfa762466@gmail.com>
2025-09-16 00:39:40 +0800

    feat(db): add customized auth schema
    1. These schema can be referenced from better-auth docs:
    https://www.better-auth.com/docs/concepts/database#core-schema
    2. Trigger that insert owner data when registered
    
    <NOTE> I don't know why better-auth set "user" instead of users...

--- backend/sql/V1__Initial.sql
+++ backend/sql/V1__Initial.sql
@@ -1,35 +1,61 @@
--- CREATE TABLE users (
--- 	id SERIAL PRIMARY KEY,
--- 	name VARCHAR(20) NOT NULL,
--- 	email TEXT,
--- 	password TEXT NOT NULL,
--- 	phone VARCHAR(15) NOT NULL,
---
--- 	is_registered_via_line BOOLEAN NOT NULL,
--- 	is_registered_via_google BOOLEAN NOT NULL,
--- 	is_active BOOLEAN NOT NULL
--- );
--- Line User
--- CREATE TABLE user_line (
--- 	id INTEGER NOT NULL PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE,
--- 	sent_from text,
--- 	user_id text
--- );
---
--- -- Google User
--- CREATE TABLE user_google (
--- 	id INTEGER NOT NULL PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE
--- );
+CREATE TABLE "user" (
+	id TEXT PRIMARY KEY,
+	name TEXT NOT NULL,
+	email TEXT NOT NULL UNIQUE,
+	"emailVerified" BOOLEAN NOT NULL,
+	image TEXT,
+	"createdAt" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	"updatedAt" TIMESTAMP NOT NULL,
+
+	"isLostAnimalMailEnabled" BOOLEAN DEFAULT FALSE,
+	"isSmsEnabled" BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE session (
+	id TEXT NOT NULL PRIMARY KEY,
+	"expiresAt" TIMESTAMP NOT NULL,
+	token TEXT NOT NULL UNIQUE,
+	"createdAt" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	"updatedAt" TIMESTAMP NOT NULL,
+	"ipAddress" TEXT,
+	"userAgent" TEXT,
+	"userId" TEXT NOT NULL REFERENCES "user"(id) ON DELETE CASCADE
+);
+
+CREATE TABLE account (
+	id TEXT NOT NULL PRIMARY KEY,
+	"accountId" TEXT NOT NULL,
+	"providerId" TEXT NOT NULL,
+	"userId" TEXT NOT NULL REFERENCES "user"(id) ON DELETE CASCADE,
+	"accessToken" TEXT,
+	"refreshToken" TEXT,
+	"idToken" TEXT,
+	"accessTokenExpiresAt" TIMESTAMP,
+	"refreshTokenExpiresAt" TIMESTAMP,
+	scope TEXT,
+	password TEXT,
+	"createdAt" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	"updatedAt" TIMESTAMP NOT NULL
+);
+
+CREATE TABLE verification (
+	id TEXT NOT NULL PRIMARY KEY,
+	identifier TEXT NOT NULL,
+	value TEXT NOT NULL,
+	expiresAt TIMESTAMP NOT NULL,
+	createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
 
 
-CREATE TABLE animal_shelters (
+CREATE TABLE animal_shelter (
 	id INTEGER PRIMARY KEY,
 	name VARCHAR(100) NOT NULL,    -- shelter_name in API
 	address VARCHAR(100) NOT NULL, -- shelter_address in API
 	tel VARCHAR(30) NOT NULL       -- shelter_tel in API
 );
 
-CREATE TABLE animals (
+CREATE TABLE animal (
 	id SERIAL NOT NULL PRIMARY KEY,
 	sub_id VARCHAR(20),
 	kind VARCHAR(20),
@@ -47,11 +73,10 @@ CREATE TABLE animals (
 	close_date DATE,
 	update_date DATE,
 
-	animal_shelter_id INTEGER NOT NULL REFERENCES animal_shelters(id) ON DELETE CASCADE
+	animal_shelter_id INTEGER NOT NULL REFERENCES animal_shelter(id) ON DELETE CASCADE
 );
 
-
-CREATE TABLE owners (
+CREATE TABLE owner (
 	id SERIAL PRIMARY KEY,
 	name TEXT,
 	phone TEXT,
@@ -59,7 +84,7 @@ CREATE TABLE owners (
 	CONSTRAINT unique_phone_email UNIQUE(phone, email)
 );
 
-CREATE TABLE animal_losts (
+CREATE TABLE animal_lost (
 	id SERIAL NOT NULL PRIMARY KEY,
 	chip_id TEXT unique,
 	name TEXT,
@@ -73,25 +98,25 @@ CREATE TABLE animal_losts (
 	lost_place TEXT,
 	picture TEXT,
 
-	owner_id INTEGER NOT NULL REFERENCES owners(id) ON DELETE CASCADE
+	owner_id INTEGER NOT NULL REFERENCES owner(id) ON DELETE CASCADE
 );
 
--- FUNCTION
--- CREATE OR REPLACE FUNCTION convert_sex_to_mf()
--- RETURNS TRIGGER AS $$
--- BEGIN
---     IF NEW.sex = '公' THEN
---        NEW.sex = 'M';
---    ELSIF NEW.sex = '母' THEN
---        NEW.sex = 'F';
---    END IF;
---
---    RETURN NEW;
--- END;
--- $$ LANGUAGE plpgsql;
-
 -- TRIGGER
--- CREATE TRIGGER convert_sex_trigger
--- BEFORE INSERT OR UPDATE ON animal_losts
--- FOR EACH ROW
--- EXECUTE FUNCTION convert_sex_to_mf();
+CREATE OR REPLACE FUNCTION handle_user_on_insert()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW."isLostAnimalMailEnabled" = (NEW.email IS NOT NULL AND NEW.email != '');
+
+    -- NEW.isSmsEnabled = (NEW.phone IS NOT NULL AND NEW.phone != '');
+
+    INSERT INTO owner(name, email)
+    VALUES (NEW.name, NEW.email);
+    
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER handle_user_on_insert
+BEFORE INSERT ON "user"
+FOR EACH ROW
+EXECUTE FUNCTION handle_user_on_insert();
