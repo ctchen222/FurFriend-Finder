26f567ef2c665ef3954d4dedbf19bd693670df0e
CHENG-TING CHEN <abfa762466@gmail.com>
2025-09-23 23:56:22 +0800

    feat: webhook route and controller
    contrler deal with postback event sent from line user, we still got one
    event to handle

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,14 +1,17 @@
 import express from 'express';
 import { router as animalRoute } from './animalRouter';
 import { router as viewRoute } from './viewRouter';
-import { router as animalLostRoute } from './animalLostRouter'; // 引入新的 router
+import { router as animalLostRoute } from './animalLostRouter';
 import { router as authRoute } from './authRouter';
+import { router as webhookRoute } from './webhookRouter'
 import { addUserToLocals } from '../middleware/userSession';
 
 export default function routes(app: express.Express) {
 	app.use("/api/animals", animalRoute)
 	app.use("/api/lost-animals", animalLostRoute)
 	app.use("/api/auth", addUserToLocals, authRoute)
+
+	app.use("/webhook", webhookRoute)
 
 	// View rendering
 	app.use("/", addUserToLocals, viewRoute)
